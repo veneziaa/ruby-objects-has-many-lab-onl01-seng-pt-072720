e62014eaeab6ed6d9073de609c504eee0ad011d7
Venice Vargas <venicevargas1@live.com>
2020-09-19 06:51:35 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,23 @@
 class Post
-  attr_accessor :author, :title 
+  attr_accessor :title, :author
+
+  @@all = []
 
   def initialize(title)
     @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def author_name
-    if author 
-      self.author.name 
+    if self.author
+      self.author.name
     else
       nil
     end
   end
 
-end 
+end

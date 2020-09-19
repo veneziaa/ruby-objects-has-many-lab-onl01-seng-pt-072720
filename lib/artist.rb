8414632669a1d26774b0d4204bc72dce23b3fd74
Venice Vargas <venicevargas1@live.com>
2020-09-19 06:38:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,29 @@
-class Artist
 
-  attr_accessor :name, :songs
+class Author
 
-  @@song_count = 0
+  attr_accessor :name, :posts
 
+  @@post_count = 0
+  
   def initialize(name)
     @name = name
-    @songs = []
+    @posts = []
   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
   end
 
-  def self.song_count
-    @@song_count
+  def self.post_count
+    @@post_count
   end
-
 end

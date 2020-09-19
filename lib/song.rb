af878766bdf9f1124d0f547d6478728f5d61303a
Venice Vargas <venicevargas1@live.com>
2020-09-19 06:51:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,23 @@
 class Song
-  attr_accessor :artist, :name
+  attr_accessor :name, :artist
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def artist_name
-    if artist
+    if self.artist
       self.artist.name
     else
       nil
     end
   end
 
-end 
+end

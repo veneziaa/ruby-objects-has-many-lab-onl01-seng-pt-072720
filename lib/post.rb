5230a88b48c58f9bc5a206f43cf129ac97f9baed
Venice Vargas <venicevargas1@live.com>
2020-09-19 06:48:04 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,30 +1,16 @@
-lass Author 
-  attr_accessor :name
-  @@post_count = 0
+class Post
+  attr_accessor :author, :title 
 
-  def initialize(name)
-    @name = name
-    @posts = []
+  def initialize(title)
+    @title = title
   end
 
-  def posts
-    @posts
+  def author_name
+    if author 
+      self.author.name 
+    else
+      nil
+    end
   end
 
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def self.post_count
-    @@post_count
-  end
 end 
